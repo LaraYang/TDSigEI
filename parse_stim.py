ef76ad34d21fcc5df2ef58ed31e002e1ff9d87ea
Ruoying Yang <Lara@Ruoyings-MacBook-Pro-2.local>
2015-11-05 16:36:57 -0800

    changed subjID to 539

--- parse_stim.py
+++ parse_stim.py
@@ -7,11 +7,11 @@ import numpy as np
 import os.path
 
 #parse block order
-os.chdir('/home/despoB/kaihwang/TRSE/TDSigEI')
+os.chdir('/home/despoB/TRSEPPI/TDSigEI')
 #Subjects = glob.glob('5*')
 #Subjects =[503, 505, 508, 509, 512, 513, 516, 517, 518, 523, 527]
-Subjects = [503]
-os.chdir('/home/despoB/kaihwang/TRSE/TDSigEI/Scripts/Logs')
+Subjects = [539]
+os.chdir('/home/despoB/TRSEPPI/TDSigEI/Scripts/Logs')
 
 for s in Subjects:
 
