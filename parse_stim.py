b2f5122a40d3ff0d0f343fa865125d450056219f
Lara Yang (via kaihwang) <lyang@nx3.priv>
2015-11-21 11:36:32 -0800

    parsing second batch

--- parse_stim.py
+++ parse_stim.py
@@ -9,9 +9,8 @@ import os.path
 #parse block order
 # os.chdir("/home/despoB/TRSEPPI/TDSigEI")
 #Subjects = glob.glob('5*')
-<<<<<<< HEAD
 #Subjects =[503, 505, 508, 509, 512, 513, 516, 517, 518, 523, 527]
-Subjects = [539]
+Subjects = [545]
 os.chdir("/home/despoB/TRSEPPI/TDSigEI/Scripts/Logs")
 
 for s in Subjects:
